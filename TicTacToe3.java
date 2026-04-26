23786552bf3f065fc389906b1ca0a198f560cbca
sudhanvesh <sudhanvesh@gmail.com>
2026-04-27 00:14:39 +0530

    Add files via upload

---
+++ TicTacToe3.java
@@ -0,0 +1,16 @@
+public class TicTacToe3 {
+
+    public static void main(String[] args) {
+        int slot = 7;
+        System.out.println("Row: " + getRowFromSlot(slot));
+        System.out.println("Column: " + getColFromSlot(slot));
+    }
+
+    static int getRowFromSlot(int slot) {
+        return (slot - 1) / 3;
+    }
+
+    static int getColFromSlot(int slot) {
+        return (slot - 1) % 3;
+    }
+}
