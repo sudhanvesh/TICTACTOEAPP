3f86eb207c6a5ca752ee6216308266926e00989c
sudhanvesh <sudhanvesh@gmail.com>
2026-04-27 00:05:53 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human plays first");
+        } else {
+            System.out.println("Computer plays first");
+        }
+
+        System.out.println("Human symbol: " + humanSymbol);
+        System.out.println("Computer symbol: " + computerSymbol);
+    }
+}
